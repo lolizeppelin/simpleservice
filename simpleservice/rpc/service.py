38f85c19ce1483744c3afe4dbea729d663d4edb1
lolizeppelin <lolizeppelin@gmail.com>
2017-05-04 19:11:57 +0800

    删除不必要注释

--- simpleservice/rpc/service.py
+++ simpleservice/rpc/service.py
@@ -31,8 +31,6 @@ class LauncheRpcServiceBase(LauncheServiceBase):
     """
 
     def __init__(self, binary, topic, manager, endpoints,
-                 # report_interval=None,
-                 periodic_interval=None, periodic_fuzzy_delay=None,
                  *args, **kwargs):
 
         self.binary = binary
@@ -43,10 +41,6 @@ class LauncheRpcServiceBase(LauncheServiceBase):
 
         for endpoint_name in endpoints:
             self.endpoints.append(importutils.import_class(endpoint_name)(host=CONF.host, *args, **kwargs))
-
-        # self.report_interval = report_interval
-        self.periodic_interval = periodic_interval
-        self.periodic_fuzzy_delay = periodic_fuzzy_delay
         self.saved_args, self.saved_kwargs = args, kwargs
         self.timers = []
         LauncheServiceBase.__init__(self, self.binary)
@@ -59,25 +53,14 @@ class LauncheRpcServiceBase(LauncheServiceBase):
         LOG.debug("Creating Consumer connection for Service %s",
                   self.topic)
         # self.conn.create_consumer(self.topic, self.endpoints)
-        if callable(getattr(self.manager, 'initialize_service_hook', None)):
-            self.manager.initialize_service_hook(self)
+        self.manager.initialize_service_hook(self)
         self.conn.start()
 
-        # if self.report_interval:
-        #     pulse = loopingcall.FixedIntervalLoopingCall(self.report_state)
-        #     pulse.start(interval=self.report_interval,
-        #                 initial_delay=self.report_interval)
-        #     self.timers.append(pulse)
-
-        if self.periodic_interval:
-            if self.periodic_fuzzy_delay:
-                initial_delay = random.randint(0, self.periodic_fuzzy_delay)
-            else:
-                initial_delay = None
-
-            periodic = loopingcall.FixedIntervalLoopingCall(self.manager.periodic_tasks)
-            periodic.start(interval=self.periodic_interval,
-                           initial_delay=initial_delay)
+        # task must callable
+        for task in self.manager.periodic_tasks:
+            periodic = loopingcall.FixedIntervalLoopingCall(task)
+            periodic.start(interval=task.periodic_interval,
+                           initial_delay=task.initial_delay)
             self.timers.append(periodic)
 
         self.manager.after_start()
@@ -96,6 +79,7 @@ class LauncheRpcServiceBase(LauncheServiceBase):
         # This function will call by Launcher from outside
         try:
             self.conn.close()
+            self.manager.after_stop()
         except Exception:
             pass
         for x in self.timers:
@@ -103,7 +87,7 @@ class LauncheRpcServiceBase(LauncheServiceBase):
                 x.stop()
             except Exception:
                 LOG.exception("Exception occurs when timer stops")
-        self.timers = []
+        del self.timers[:]
         del self.endpoints[:]
 
     def wait(self):
@@ -112,18 +96,6 @@ class LauncheRpcServiceBase(LauncheServiceBase):
                 x.wait()
             except Exception:
                 LOG.exception("Exception occurs when waiting for timer")
-
-    # def periodic_tasks(self, raise_on_error=False):
-    #     """Tasks to be run at a periodic interval."""
-    #     ctxt = context.get_admin_context()
-    #     self.manager.periodic_tasks(ctxt, raise_on_error=raise_on_error)
-
-    # def report_state(self):
-    #     """Update the state of this service."""
-    #     pass
-
-    # def reset(self):
-    #     config.reset_service()
 
 
 @singleton
