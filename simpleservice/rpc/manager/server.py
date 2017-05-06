875a6f98897f2e05491b566b49205e00f1d90dbb
lolizeppelin <lolizeppelin@gmail.com>
2017-05-06 13:29:30 +0800

    基本函数

--- simpleservice/rpc/manager/server.py
+++ simpleservice/rpc/manager/server.py
@@ -1,3 +1,4 @@
+from simpleutil.utils.timeutils import realnow as now
 
 from simpleutil.config import cfg
 from simpleutil.log import log as logging
@@ -7,6 +8,7 @@ from simpleservice.rpc import common
 from simpleservice.rpc.target import Target
 from simpleservice.rpc.manager.base import ManagerBase
 
+
 CONF = cfg.CONF
 
 LOG = logging.getLogger(__name__)
@@ -14,4 +16,25 @@ LOG = logging.getLogger(__name__)
 class ServerManager(ManagerBase):
 
     def __init__(self):
-        self.target = Target(topic=common.AGENT, namespace='manager', server=CONF.host)
+        ManagerBase.__init__(self, 'manager')
+        self.target = Target(topic=common.AGENT,
+                             namespace=self.namespace)
+
+    def init_host(self):
+        if self.target.server is None:
+            self.target.server = CONF.host
+
+    def initialize_service_hook(self, rpcservice):
+        # check endpoint here
+        pass
+
+    def call_endpoint(self, endpoint, method, ctxt, args):
+        return {}
+
+    def full(self):
+        return False
+
+    def rpc_show(self, ctxt, **kwargs):
+        print ctxt
+        print kwargs
+        return {'ret': 'rpc show success'}
