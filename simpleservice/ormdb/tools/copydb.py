dc954137e06a6e1f4e66e876ab0f8338f74ae5ee
lolizeppelin <lolizeppelin@gmail.com>
2017-05-17 19:48:51 +0800

    数据库初始化使用统一的init_database函数,复制表数据和sql执行可用

--- simpleservice/ormdb/tools/copydb.py
+++ simpleservice/ormdb/tools/copydb.py
@@ -1,20 +1,27 @@
 from sqlalchemy import MetaData
+from sqlalchemy import func
 from sqlalchemy.pool import NullPool
 from sqlalchemy.exc import OperationalError
 
+from simpleservice.ormdb import orm
 from simpleservice.ormdb.engines import create_engine
 from simpleservice.ormdb.argformater import connformater
 from simpleservice.ormdb.exceptions import AcceptableError
 
-from simpleservice.ormdb.tools.exceptions import DropCreateedDBFail
-from simpleservice.ormdb.tools.exceptions import DBExist
+from simpleservice.ormdb.tools import exceptions
 from simpleservice.ormdb.tools.utils import get_schema_info
-from simpleservice.ormdb.tools.utils import create_schema
-from simpleservice.ormdb.tools.utils import drop_schema
+from simpleservice.ormdb.tools.utils import init_database
 
 
-def copydb(src, dst, table_with_data=None, exec_sql=None):
-    meta = MetaData()
+MAX_COPY_ROW = 100000
+
+
+def copydb(src, dst, tables_need_copy=None, exec_sqls=None):
+    """copy database from src to dst
+    tables_need_copy include table that need copy data
+    exec_sqls include list of sql should be run after copy"""
+    init_data = None
+    metadata = MetaData()
     src_connection = connformater % src
     dst_connection = connformater % dst
     src_engine = create_engine(src_connection, thread_checkin=False,
@@ -25,24 +32,39 @@ def copydb(src, dst, table_with_data=None, exec_sql=None):
         schema_info = get_schema_info(src_engine)
         if not schema_info:
             raise AcceptableError('Get source database error: %s not exist' % src_engine.url.database)
-        meta.reflect(bind=src_engine)
+        metadata.reflect(bind=src_engine)
     except OperationalError as e:
         raise AcceptableError('Get source database info or Reflect source database error:%d, %s' %
                               (e.orig[0], e.orig[1].replace("'", '')))
-    try:
-        create_schema(dst_engine, charcter_set=schema_info[1],
-                      collation_type=schema_info[2])
-    except OperationalError as e:
-        raise AcceptableError('Create distribution database error:%d, %s' %
-                              (e.orig[0], e.orig[1].replace("'", '')))
-    except DBExist as e:
-        raise AcceptableError('Create distribution database error: %s' % e.message)
-    try:
-        meta.create_all(bind=dst_engine)
-    except OperationalError as e:
-        try:
-            drop_schema(dst_engine)
-        except:
-            raise DropCreateedDBFail('Copy table fail, Drop database fail', dst_connection)
-        raise AcceptableError('Copy table into distribution database error:%d, %s'
-                              % (e.orig[0], e.orig[1].replace("'", '')))
+    if tables_need_copy or exec_sqls:
+
+        def init_data(*args):
+            dst_session = orm.get_maker(dst_engine)()
+            src_session = orm.get_maker(src_engine)()
+            if tables_need_copy:
+                count = 0
+                for table in tables_need_copy:
+                    if table not in metadata.tables:
+                        raise AcceptableError('Table %s not in source database' % table)
+                for table_name in tables_need_copy:
+                    table = metadata.tables[table_name]
+                    # get row count from table
+                    count += src_session.query(func.count("*")).select_from(table).scalar()
+                    if count >= MAX_COPY_ROW:
+                        raise exceptions.CopyRowOverSize('Copy from table %s fail, too many rows copyed' %
+                                                         table_name)
+                    # build a query in src database
+                    query = src_session.query(table)
+                    with dst_session.begin():
+                        for data in query:
+                            # execute insert sql on dst databases
+                            dst_session.execute(table.insert(data))
+            if exec_sqls:
+                with dst_session.begin():
+                    for sql in exec_sqls:
+                        dst_session.execute(sql)
+
+    init_database(dst_engine, metadata,
+                  charcter_set=schema_info[1],
+                  collation_type=schema_info[2],
+                  init_data_func=init_data)
