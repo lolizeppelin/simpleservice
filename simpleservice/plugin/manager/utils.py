6b34fbb90064c05e9f4cf630d08621b8b5adbd14
lolizeppelin <lolizeppelin@gmail.com>
2017-05-17 20:34:16 +0800

    PE8空格增加

--- simpleservice/plugin/manager/utils.py
+++ simpleservice/plugin/manager/utils.py
@@ -4,6 +4,7 @@ from simpleservice.ormdb.tools.utils import init_database
 from simpleservice.plugin.manager.models import ManagerTableBase
 from simpleservice.plugin.manager.models import GkeyMap
 
+
 def init_manager_data(engine):
     session_maker = orm.get_maker(engine=engine)
     session = session_maker()
