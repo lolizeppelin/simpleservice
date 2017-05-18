eafdbba57bf0766638d1cb7a862a3b55fc6462d7
lolizeppelin <lolizeppelin@gmail.com>
2017-05-18 16:26:44 +0800

    asyncrequest路由编写

--- simpleservice/plugin/manager/wsgi/asyncrequest/routers.py
+++ simpleservice/plugin/manager/wsgi/asyncrequest/routers.py
@@ -1,11 +1,10 @@
 from simpleservice.wsgi import router
-from simpleservice.wsgi import controller_return_response
+from simpleservice.wsgi.middleware import controller_return_response
 
 from simpleservice.plugin.manager.wsgi.asyncrequest import controller
 
 MEMBER_ACTIONS = ['show']
 COLLECTION_ACTIONS = ['index']
-
 
 class Routers(router.RoutersBase):
     collection_name = 'asyncrequests'
