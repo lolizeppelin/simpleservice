779a3ba7259e094043d745000b64e1daccc1f461
lolizeppelin <lolizeppelin@gmail.com>
2017-05-18 16:24:59 +0800

    定义核心route

--- simpleservice/plugin/__init__.py
+++ simpleservice/plugin/__init__.py
@@ -1,7 +1,7 @@
 from simpleservice.plugin.manager.rpc.base import ManagerBase
 
-from simpleservice.plugin.manager.wsgi.agent.routers import Routers as agent_routes
-from simpleservice.plugin.manager.wsgi.asyncrequest.routers import Routers as request_routes
-from simpleservice.plugin.endpoint.wsgi.scheduler.routers import Routers as scheduler_routes
+from simpleservice.plugin.manager.wsgi.agent import routers as agent_routes
+from simpleservice.plugin.manager.wsgi.asyncrequest import routers as request_routes
+from simpleservice.plugin.endpoint.wsgi.scheduler import routers as scheduler_routes
 
 CORE_ROUTES = [request_routes, agent_routes, scheduler_routes]
