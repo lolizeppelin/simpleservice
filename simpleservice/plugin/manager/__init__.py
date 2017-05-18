aaf6f778737aa70c855e9d6c38e5e100ed0b286e
lolizeppelin <lolizeppelin@gmail.com>
2017-05-18 16:24:43 +0800

    数据库连接获取方法写入__init__文件中

--- simpleservice/plugin/manager/__init__.py
+++ simpleservice/plugin/manager/__init__.py
@@ -1,24 +1,36 @@
+from simpleutil.config import cfg
+
 from simpleutil.log import log as logging
+from simpleservice.ormdb.config import database_opts
 from simpleservice.ormdb.api import MysqlDriver
 
 LOG = logging.getLogger(__name__)
 
+CONF = cfg.CONF
+
 DbDriver = None
 
+manager_group = cfg.OptGroup(name='manager', title='Manager options')
+CONF.register_group(manager_group)
 
-def init_session(name, conf):
+def init_session():
+    global DbDriver
     if DbDriver is None:
-        mysql_driver = MysqlDriver(name, conf)
+        LOG.info("Try connect database for manager")
+        CONF.register_opts(database_opts, manager_group)
+        mysql_driver = MysqlDriver(manager_group.name,
+                                   CONF[manager_group.name])
         mysql_driver.start()
-        global DbDriver
         DbDriver = mysql_driver
     else:
         LOG.warning("Do not call init_session more then once")
 
 
 def get_session(readonly=False):
+    global DbDriver
     if DbDriver is None:
-        raise RuntimeError('Database not connected')
+        init_session()
+        # raise RuntimeError('Database not connected')
     if readonly:
         return DbDriver.rsession()
     return DbDriver.session()
