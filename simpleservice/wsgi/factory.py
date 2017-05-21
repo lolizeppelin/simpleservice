e77fac976893fb6b3864c52dd773e633bcbeb9a1
lolizeppelin <lolizeppelin@gmail.com>
2017-05-21 18:02:55 +0800

    factory函数对应修改

--- simpleservice/wsgi/factory.py
+++ simpleservice/wsgi/factory.py
@@ -1,12 +1,10 @@
 import functools
 import sys
 import routes
-import copy
 
 from simpleutil.log import log
 
 from simpleservice.wsgi import router
-from simpleservice.plugin import CORE_ROUTES
 
 
 LOG = log.getLogger(__name__)
@@ -26,7 +24,7 @@ def fail_gracefully(f):
     return wrapper
 
 
-def app_factory(project_routers=None):
+def app_factory(project_routers):
     @fail_gracefully
     def wrapper_factory(global_conf, **local_conf):
         mapper = routes.Mapper()
@@ -34,10 +32,7 @@ def app_factory(project_routers=None):
         # NOTE(dstanek): Routers should be ordered by their frequency of use in
         # a live system. This is due to the routes implementation. The most
         # frequently used routers should appear first.
-        all_api_routers = copy.copy(CORE_ROUTES)
-        if project_routers:
-            all_api_routers.extend(project_routers)
-        all_api_routers = set(all_api_routers)
+        all_api_routers = set(project_routers)
         for api_routers in all_api_routers:
             api_routers.Routers().append_routers(mapper, sub_routers)
         return router.ComposingRouter(mapper, sub_routers)
