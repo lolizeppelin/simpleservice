3c64b4d1438b4c08ac4f8607e3bad66536afb7b2
lolizeppelin <lolizeppelin@gmail.com>
2017-07-19 10:40:05 +0800

    标准错误为负数,正数留给外部定义用

--- simpleservice/common.py
+++ simpleservice/common.py
@@ -2,6 +2,6 @@
 RESULT_UNKNOWN = -127
 RESULT_IS_NONE = -126
 RESULT_OVER_DEADLINE = -125
+RESULT_ERROR = -1
 RESULT_SUCCESS = 0
-RESULT_ERROR = 1
-# result over 1 is coumse error
+# result over 0 is coumse error
