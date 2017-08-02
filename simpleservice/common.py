10c3c3cb9138d33449c8e19ab81d1bdf4cda89e1
lolizeppelin <lolizeppelin@gmail.com>
2017-08-02 12:35:27 +0800

    增加两个错误码, 注释单词错误修正

--- simpleservice/common.py
+++ simpleservice/common.py
@@ -2,6 +2,8 @@
 RESULT_UNKNOWN = -127
 RESULT_IS_NONE = -126
 RESULT_OVER_DEADLINE = -125
+DESERIALIZE_FAIL = -3
+SERIALIZE_FAIL = -2
 RESULT_ERROR = -1
 RESULT_SUCCESS = 0
-# result over 0 is coumse error
+# result over 0 is customer error
