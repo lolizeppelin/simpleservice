e5dbd1f0fcbe2101797055bda00feb2651cb51f6
lolizeppelin <lolizeppelin@gmail.com>
2017-08-07 12:04:53 +0800

    统一系统类型判断

--- simpleservice/wsgi/service.py
+++ simpleservice/wsgi/service.py
@@ -8,10 +8,10 @@ import greenlet
 
 from paste import deploy
 
+from simpleutil import system
 from simpleutil.config import cfg
 from simpleutil.log import log as logging
 from simpleutil.common.exceptions import InvalidInput
-from simpleutil.posix.linux import set_cloexec_flag
 
 from simpleservice.base import LauncheServiceBase
 from simpleservice.wsgi.exceptions import ConfigNotFound
@@ -226,6 +226,8 @@ class LauncheWsgiServiceBase(LauncheServiceBase):
             LOG.info("WSGI server has stopped.")
 
     def close_exec(self):
-        if self.dup_socket:
-            set_cloexec_flag(self.dup_socket.fileno())
-        set_cloexec_flag(self.socket.fileno())
+        if system.LINUX:
+            from simpleutil.posix.linux import set_cloexec_flag
+            if self.dup_socket:
+                set_cloexec_flag(self.dup_socket.fileno())
+            set_cloexec_flag(self.socket.fileno())
