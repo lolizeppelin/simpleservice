306ca919cdb70d15f49cc09424a7c2d4d0ef7250
lolizeppelin <lolizeppelin@gmail.com>
2017-08-17 17:09:38 +0800

    rpc cast 的超时时间也收到配置影响而不再使用heartbeat的时间

--- simpleservice/rpc/client.py
+++ simpleservice/rpc/client.py
@@ -16,20 +16,20 @@ DEFAULT_LOG_AFTER = 30
 class RPCClientBase(object):
 
     def __init__(self, conf):
+        self.conf = conf
         self.rpcdriver = RabbitDriver(conf)
-        self.retry = conf.rpc_send_retry
 
     def notify(self, target, ctxt, msg):
         try:
-            self.rpcdriver.send_notification(target, ctxt, msg, retry=self.retry)
+            self.rpcdriver.send_notification(target, ctxt, msg, retry=self.conf.rabbit_send_retry)
         except exceptions.RabbitDriverError as ex:
             raise exceptions.RpcClientSendError(target, ex)
 
     def cast(self, target, ctxt, msg):
         """Invoke a method and return immediately. See RPCClient.cast()."""
         try:
-            self.rpcdriver.send(target, ctxt, msg,
-                                retry=self.retry)
+            self.rpcdriver.send(target, ctxt, msg, timeout=self.conf.rpc_send_timeout,
+                                retry=self.conf.rpc_send_retry)
         except exceptions.RabbitDriverError as ex:
             raise exceptions.RpcClientSendError(target, ex)
 
@@ -38,7 +38,8 @@ class RPCClientBase(object):
             raise exceptions.InvalidTarget('A call cannot be used with fanout', target)
         try:
             return self.rpcdriver.send(target, ctxt, msg,
-                                       wait_for_reply=True, timeout=timeout,
-                                       retry=self.retry)
+                                       wait_for_reply=True,
+                                       timeout=timeout if timeout else self.conf.rpc_send_timeout,
+                                       retry=self.conf.rpc_send_retry)
         except exceptions.RabbitDriverError as ex:
             raise exceptions.RpcClientSendError(target, ex)
