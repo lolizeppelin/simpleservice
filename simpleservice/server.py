c1cc0bf188a0cde7624c2b4e00ff86422a4c8e66
lolizeppelin <lolizeppelin@gmail.com>
2017-08-18 11:46:12 +0800

    变更变量名字便于阅读

--- simpleservice/server.py
+++ simpleservice/server.py
@@ -16,47 +16,46 @@ CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
 
-class ServerWrapper(object):
+class LaunchWrapper(object):
     """Wraps a Server with some launching info & capabilities."""
 
-    def __init__(self, server, workers):
-        self.server = server
+    def __init__(self, service, workers):
+        self.service = service
         self.workers = workers
 
     def launch_with(self, launcher):
-        # self.server.listen()
         if self.workers > 1:
             # Use multi-process launcher
-            launcher.launch_service(self.server, self.workers)
+            launcher.launch_service(self.service, self.workers)
         else:
             # Use single process launcher
-            launcher.launch_service(self.server)
+            launcher.launch_service(self.service)
 
 
-def launch(servers, user='root', group='root'):
+def launch(wrappers, user='root', group='root'):
     CONF.register_opts(ntp_opts)
     if CONF.ntp_server:
         from simpleutil.utils.timeutils import ntptime
         if abs(ntptime(CONF.ntp_server, CONF.ntp_versio, CONF.ntp_port, CONF.ntp_timeout).offset) >= 1.0:
             raise RuntimeError('Ntp offset more then 1 second, Please sync time first before launch')
-    if max([server.workers for server in servers]) > 1:
+    if max([wrapper.workers for wrapper in wrappers]) > 1:
         if not system.POSIX:
             raise RuntimeError('ProcessLauncher just for posix system')
         launcher = ProcessLauncher(CONF)
     else:
         launcher = ServiceLauncher(CONF)
     # 根据情况启动守护进程
-    systemd.daemon(pidfile=os.path.join(CONF.state_path, '%s.lock' % servers[0].server.name),
+    systemd.daemon(pidfile=os.path.join(CONF.state_path, '%s.lock' % wrappers[0].service.name),
                    user=user, group=group)
-    for server in servers:
+    for wrapper in wrappers:
         try:
-            server.launch_with(launcher)
+            wrapper.launch_with(launcher)
         except socket.error:
             LOG.exception('Failed to start the %(name)s server' % {
-                'name': server.server.name})
+                'name': wrapper.service.name})
             raise
     # notify calling process we are ready to serve
     systemd.notify_once()
     launcher.wait()
-    for server in servers:
-        LOG.info('Stoped %(name)s server' % {'name': server.server.name})
+    for wrapper in wrappers:
+        LOG.info('Stoped %(name)s server' % {'name': wrapper.service.name})
