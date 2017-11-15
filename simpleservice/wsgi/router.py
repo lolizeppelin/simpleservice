5dafd17fd00512709fe47b3878ca22c10afc0df3
lolizeppelin <lolizeppelin@gmail.com>
2017-11-15 17:25:16 +0800

    还原代码

--- simpleservice/wsgi/router.py
+++ simpleservice/wsgi/router.py
@@ -70,11 +70,11 @@ class ComposableRouter(Router):
     这个类要继承重写
     """
 
-    # def __init__(self, mapper=None):
-    #     if mapper is None:
-    #         mapper = routes.Mapper()
-    #     self.add_routes(mapper)
-    #     super(ComposableRouter, self).__init__(mapper)
+    def __init__(self, mapper=None):
+        if mapper is None:
+            mapper = routes.Mapper()
+        self.add_routes(mapper)
+        super(ComposableRouter, self).__init__(mapper)
 
     @abc.abstractmethod
     def add_routes(self, mapper):
