d90499fb682bc9f08e747b0e413af966461cf9a8
lolizeppelin <lolizeppelin@gmail.com>
2017-12-15 18:25:00 +0800

    copydb代码合并到utils中 option支持迭代对象

--- simpleservice/ormdb/tools/utils.py
+++ simpleservice/ormdb/tools/utils.py
@@ -1,4 +1,5 @@
 import copy
+import contextlib
 
 from simpleutil.config import cfg
 from simpleutil.utils import jsonutils
@@ -11,6 +12,7 @@ from sqlalchemy.engine.base import Engine
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.exc import SQLAlchemyError
 
+from simpleservice.ormdb import orm
 from simpleservice.ormdb.engines import create_engine
 from simpleservice.ormdb.argformater import connformater
 from simpleservice.ormdb.exceptions import AcceptableError
@@ -69,6 +71,20 @@ def get_schema_info(engine):
     return None
 
 
+def create_privileges(engine, auths):
+    jsonutils.schema_validate(auths, AUTHSCHEMA)
+    schema = engine.url.database
+    no_schema_engine = get_no_schema_engine(engine)
+    for auth in auths:
+        _auth = {'schema': schema,
+                 'user': auth.get('user'),
+                 'source': auth.get('source') or '%',
+                 'privileges': auth.get('privileges') or 'ALL'}
+        sql = "GRANT %(privileges)s ON %(schema)s.* TO '%(user)s'@'%(source)s'" % _auth
+        no_schema_engine.execute(sql)
+    no_schema_engine.execute('FLUSH PRIVILEGES')
+
+
 # drop privileges
 def drop_privileges(engine, auths):
     jsonutils.schema_validate(auths, AUTHSCHEMA)
@@ -78,10 +94,23 @@ def drop_privileges(engine, auths):
         _auth = {'schema': schema,
                  'user': auth.get('user'),
                  'source': auth.get('source') or '%',
-                 'privileges': auth.get('privileges') or 'ALL PRIVILEGES'}
+                 'privileges': auth.get('privileges') or 'ALL'}
         sql = "REVOKE %(privileges)s ON %(schema)s.* FROM '%(user)s'@'%(source)s'" % _auth
         no_schema_engine.execute(sql)
     no_schema_engine.execute('FLUSH PRIVILEGES')
+
+
+@contextlib.contextmanager
+def privileges(engine, auths):
+    if not auths:
+        yield
+    else:
+        try:
+            create_privileges(engine, auths)
+            yield
+        except Exception:
+            drop_privileges(engine, auths)
+            raise
 
 
 # create a schema
@@ -97,9 +126,9 @@ def create_schema(engine, auths=None, charcter_set=None, collation_type=None):
     sql = "CREATE DATABASE %s DEFAULT CHARACTER SET %s" % (schema, charcter_set)
     if collation_type:
         sql += ' COLLATE %s' % collation_type
-    no_schema_engine.execute(sql)
-    if auths:
-        drop_privileges(engine, auths)
+    with privileges(engine, auths):
+        no_schema_engine.execute(sql)
+
 
 # drop a schema
 def drop_schema(engine, auths=None):
@@ -160,3 +189,67 @@ def init_database(db_info, metadata,
             raise AcceptableError('Create table error:%d, %s' %
                                   (e.orig[0], e.orig[1].replace("'", '')))
         raise AcceptableError('Create tables or insert row error: %s' % e.message)
+
+
+MAX_COPY_ROW = 100000
+
+
+def copydb(src, dst, auths=None, tables_need_copy=None, exec_sqls=None):
+    """copy database from src to dst
+    tables_need_copy include table that need copy data
+    exec_sqls include list of sql should be run after copy"""
+    metadata = sa.MetaData()
+    src_connection = connformater % src
+    dst_connection = connformater % dst
+    src_engine = create_engine(src_connection, thread_checkin=False,
+                               poolclass=NullPool)
+    dst_engine = create_engine(dst_connection, thread_checkin=False,
+                               poolclass=NullPool)
+    try:
+        schema_info = get_schema_info(src_engine)
+        if not schema_info:
+            raise AcceptableError('Get source database error: %s not exist' % src_engine.url.database)
+        metadata.reflect(bind=src_engine)
+    except OperationalError as e:
+        raise AcceptableError('Get source database info or Reflect source database error:%d, %s' %
+                              (e.orig[0], e.orig[1].replace("'", '')))
+
+    def init_data(*args, **kwargs):
+        if not tables_need_copy and not exec_sqls:
+            return
+        dst_session = orm.get_maker(dst_engine)()
+        src_session = orm.get_maker(src_engine)()
+        if tables_need_copy:
+            count = 0
+            for table in tables_need_copy:
+                if table not in metadata.tables:
+                    raise AcceptableError('Table %s not in source database' % table)
+            for table_name in tables_need_copy:
+                table = metadata.tables[table_name]
+                # get row count from table
+                count += src_session.query(sa.func.count("*")).select_from(table).scalar()
+                if count >= MAX_COPY_ROW:
+                    raise exceptions.CopyRowOverSize('Copy from table %s fail, too many rows copyed' %
+                                                     table_name)
+                # build a query in src database
+                query = src_session.query(table)
+                with dst_session.begin():
+                    for row in query:
+                        # execute insert sql on dst databases
+                        dst_session.execute(table.insert(row))
+        if exec_sqls:
+            with dst_session.begin():
+                for sql in exec_sqls:
+                    dst_session.execute(sql)
+        src_session.close()
+        dst_session.close()
+
+    try:
+        init_database(dst_engine, metadata, auths,
+                      charcter_set=schema_info[1],
+                      collation_type=schema_info[2],
+                      init_data_func=init_data)
+        return schema_info
+    finally:
+        del src_engine
+        del dst_engine
